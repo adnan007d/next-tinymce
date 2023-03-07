755781b11e3323d34a57a5c07141b68ea0f2f32a
adnan007d <adnanmansuri54@gmail.com>
2023-03-07 18:40:46 +0530

    initial commit

---
+++ postinstall.js
@@ -0,0 +1,11 @@
+const fse = require("fs-extra");
+const path = require("path");
+const topDir = __dirname;
+fse.emptyDirSync(path.join(topDir, "public", "tinymce"));
+console.log(path.join(topDir, "node_modules", "tinymce"));
+console.log(path.join(topDir, "public", "tinymce"));
+fse.copySync(
+  path.join(topDir, "node_modules", "tinymce"),
+  path.join(topDir, "public", "tinymce"),
+  { overwrite: true, dereference: true }
+);
