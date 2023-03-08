abb5d91773db9b487424b8e2f53da970ea31c12c
adnan007d <adnanmansuri54@gmail.com>
2023-03-08 11:37:53 +0530

    unsaved

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,54 +43,7 @@ export default function Home() {
           autosave_restore_when_empty: false,
           autosave_retention: "2m",
           image_advtab: true,
-          // link_list: [
-          //   { title: 'My page 1', value: 'https://www.tiny.cloud' },
-          //   { title: 'My page 2', value: 'http://www.moxiecode.com' }
-          // ],
-          // image_list: [
-          //   { title: 'My page 1', value: 'https://www.tiny.cloud' },
-          //   { title: 'My page 2', value: 'http://www.moxiecode.com' }
-          // ],
-          // image_class_list: [
-          //   { title: 'None', value: '' },
-          //   { title: 'Some class', value: 'class-name' }
-          // ],
           importcss_append: true,
-          // file_picker_callback: function (callback, value, meta) {
-          //   /* Provide file and text for the link dialog */
-          //   if (meta.filetype === 'file') {
-          //     callback('https://www.google.com/logos/google.jpg', { text: 'My text' });
-          //   }
-
-          //   /* Provide image and alt text for the image dialog */
-          //   if (meta.filetype === 'image') {
-          //     callback('https://www.google.com/logos/google.jpg', { alt: 'My alt text' });
-          //   }
-
-          //   /* Provide alternative source and posted for the media dialog */
-          //   if (meta.filetype === 'media') {
-          //     callback('movie.mp4', { source2: 'alt.ogg', poster: 'https://www.google.com/logos/google.jpg' });
-          //   }
-          // },
-          templates: [
-            {
-              title: "New Table",
-              description: "creates a new table",
-              content:
-                '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>',
-            },
-            {
-              title: "Starting my story",
-              description: "A cure for writers block",
-              content: "Once upon a time...",
-            },
-            {
-              title: "New list with dates",
-              description: "New List with dates",
-              content:
-                '<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>',
-            },
-          ],
           template_cdate_format: "[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]",
           template_mdate_format: "[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]",
           height: 600,
